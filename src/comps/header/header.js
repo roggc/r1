ef8e80a3d28ca6013f9128298e648c91c88aeff8
roggc <nppcpp@hotmail.com>
2019-11-01 23:52:56 +0100

    two

--- src/comps/header/header.js
+++ src/comps/header/header.js
@@ -1,16 +1,13 @@
-import React,{useRef} from 'react'
+import React from 'react'
 import {Menu} from '../menu/menu'
-import {useOuterClick} from '../../hooks/useOuterClick'
 
 export const Header=
 ({state,dispatch})=>
 {
-  const menuRef=useRef(null)
-  useOuterClick(e=>dispatch({type:'MENU_HIDE_POPUP'}),menuRef)
   const el=
   <div>
     <div>
-      <Menu state={state} dispatch={dispatch} ref={menuRef}/>
+      <Menu state={state} dispatch={dispatch}/>
     </div>
   </div>
   return el
